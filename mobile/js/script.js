e35ac295bfc50d26bf55100fed87d7c11fc905dd
kenlau5678 <63826053+kenlau5678@users.noreply.github.com>
2023-01-27 14:31:59 +0800

    Delete mobile directory

--- mobile/js/script.js
+++
@@ -1,6 +0,0 @@
-    //判断是手机还是电脑
-    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-        window.location.href = "mobile.html"; //手机
-    } else {
-        window.location.href = "index.html"; //电脑
-    }
