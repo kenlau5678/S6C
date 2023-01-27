04f28713bde4c5632e0a43189be13038d35401ee
kenlau5678 <63826053+kenlau5678@users.noreply.github.com>
2023-01-27 19:18:54 +0800

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-var mobile = document.querySelector(".mobile")
-var PC = document.querySelector(".PC")
-if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-    PC.style.display = "none"; //手机
-} else {
-    mobile.style.display = "none"; //电脑
-}
